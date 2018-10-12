38fa5712bd03afd36962b59be54d7266babd14c1
Alexander Brevig <alexanderbrevig@gmail.com>
2018-10-12 16:15:20 +0200

    Thank you roxIndie for extending with

--- GetComponents.cs
+++ GetComponents.cs
@@ -1,33 +1,74 @@
-﻿using System;
+using System;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class GetComponents : MonoBehaviour
 {
-    // Use this for initialization
-    void Awake()
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        FindAllComponentAttributes();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void FindAllComponentAttributes()
     {
         MonoBehaviour[] sceneActive = FindObjectsOfType<MonoBehaviour>();
         foreach (MonoBehaviour mono in sceneActive)
         {
             FieldInfo[] objectFields = mono.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-            for (int i = 0; i < objectFields.Length; i++)
+            foreach (var field in objectFields)
             {
-                GetComponent attribute = Attribute.GetCustomAttribute(objectFields[i], typeof(GetComponent)) as GetComponent;
+                GetComponent attribute = Attribute.GetCustomAttribute(field, typeof(GetComponent)) as GetComponent;
                 if (attribute != null)
                 {
-                    Type t = objectFields[i].FieldType;
+                    Type t = field.FieldType;
                     MethodInfo method = typeof(GameObject).GetMethod("GetComponent", BindingFlags.Instance | BindingFlags.Public, null, Type.EmptyTypes, null);
                     MethodInfo generic = method.MakeGenericMethod(t);
                     var obj = generic.Invoke(mono.gameObject, null);
                     var x = Convert.ChangeType(obj, t);
                     if (x.ToString() == "null")
                     {
-                        Debug.LogError("Could not find " + t + " on " + mono.name + " though " + objectFields[i].DeclaringType + "."+ objectFields[i].Name + " is marked [GetComponent]");
+                        Debug.LogError("Could not find " + t + " on " + mono.name + " though " + field.DeclaringType + "." + field.Name + " is marked [GetComponent]");
                     }
-                    objectFields[i].SetValue(mono, x);
+                    field.SetValue(mono, x);
                 }
             }
         }
     }
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        FindAllComponentAttributes();
+    }
 }
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+public class ExecutionSetter
+{
+    static ExecutionSetter()
+    {
+        var target = UnityEngine.Object.FindObjectsOfType(typeof(GetComponents)).FirstOrDefault();
+        if (target != null)
+        {
+            var executionOrder = -3000;
+            MonoScript script = MonoScript.FromMonoBehaviour(target as MonoBehaviour);
+            if (MonoImporter.GetExecutionOrder(script) != executionOrder)
+            {
+                MonoImporter.SetExecutionOrder(script, executionOrder);
+            }
+        }
+    }
+}
+#endif
